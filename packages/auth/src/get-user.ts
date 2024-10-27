ae5200e36dd2eaa02096525b42bcf8d8d9daebfe
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-27 22:58:33 +0600

    feat: add get-user function

---
+++ packages/auth/src/get-user.ts
@@ -0,0 +1,15 @@
+import { lucia } from ".";
+
+export async function getUser(cookieHeader: string) {
+  const sessionId = lucia.readSessionCookie(cookieHeader);
+
+  if (sessionId) {
+    const { session, user } = await lucia.validateSession(sessionId);
+
+    return { session, user }
+  }
+  return {
+    session: null,
+    user: null,
+  }
+}
