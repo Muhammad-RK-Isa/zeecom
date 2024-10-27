61236d2ec480ba2ab30447ab1cb70c2ad81ef886
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-27 12:40:11 +0600

    feat: add eslint rules for t3-env

--- apps/server/eslint.config.js
+++ apps/server/eslint.config.js
@@ -1,7 +1,8 @@
-import baseConfig from '@zeecom/eslint-config/base';
+import baseConfig, { restrictEnvAccess } from '@zeecom/eslint-config/base';
 
 /** @type {import('typescript-eslint').Config} */
 export default [
   { ignores: ["dist"] },
   ...baseConfig,
+  ...restrictEnvAccess,
 ];
