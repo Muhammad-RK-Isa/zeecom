b7fe0c8e35046396d55389f0544b96796988070d
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-27 18:46:37 +0600

    feat: add root header

---
+++ apps/admin/src/components/root-header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SidebarTrigger } from '~/components/ui/sidebar'
+
+export default function RootHeader() {
+  return (
+    <header className='border-b w-full sticky z-50 top-0'>
+      <nav className='p-4'>
+        <SidebarTrigger />
+      </nav>
+    </header>
+  )
+}
