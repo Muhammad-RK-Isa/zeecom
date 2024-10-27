b061b75dcc89fe10d766c33aab76148381dbb048
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-27 18:44:14 +0600

    feat: add admin signOut procedure

--- packages/api/src/admin/routers/auth/index.ts
+++ packages/api/src/admin/routers/auth/index.ts
@@ -1,14 +1,28 @@
 import { signInSchema, signUpSchema } from "@zeecom/validators/admin";
 
-import { createAdminRouter, publicAdminProcedure } from "../../trpc";
+import { publicAdminProcedure } from "../../trpc";
 import { signIn } from "./sign-in";
 import { signUp } from "./sign-up";
+import { TRPCRouterRecord } from "@trpc/server";
+import { lucia } from "@zeecom/auth";
 
-export const authRouter = createAdminRouter({
+export const authRouter = {
   signIn: publicAdminProcedure
     .input(signInSchema)
     .mutation(({ ctx, input }) => signIn(ctx, input)),
   signUp: publicAdminProcedure
     .input(signUpSchema)
     .mutation(({ ctx, input }) => signUp(ctx, input)),
-});
+  signOut: publicAdminProcedure.mutation(async ({ ctx }) => {
+    if (!ctx.session) return;
+    await lucia.invalidateSession(ctx.session.id);
+    ctx.resHeaders.append(
+      "Set-Cookie",
+      lucia.createBlankSessionCookie().serialize(),
+    );
+    return { success: true };
+  }),
+  getUser: publicAdminProcedure.query(({ ctx }) => {
+    return { user: ctx.user }
+  }),
+} satisfies TRPCRouterRecord
