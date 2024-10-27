1355d8d769f997f21d2f5c2f6aea24accd6976ae
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-27 18:46:26 +0600

    feat: add root sidebar

---
+++ apps/admin/src/components/root-sidebar/nav-user.tsx
@@ -0,0 +1,139 @@
+import {
+  ChevronsUpDown,
+  LogOut,
+  Moon,
+  Sun,
+  SunMoon,
+} from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu"
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "~/components/ui/sidebar"
+import { useTheme } from "~/components/theme-provider"
+import { api, trpcQueryUtils } from "~/router"
+import { useLocation, useNavigate, useRouter } from "@tanstack/react-router"
+
+export function NavUser() {
+  const router = useRouter()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const { isMobile } = useSidebar()
+  const { theme, setTheme } = useTheme()
+
+  const { data } = api.auth.getUser.useQuery()
+  const { mutate } = api.auth.signOut.useMutation({
+    onSuccess: async () => {
+      await trpcQueryUtils.auth.getUser.invalidate()
+      await router.invalidate()
+      await navigate({ to: "/sign-in", search: { callbackUrl: location.href } })
+    },
+  })
+
+  const user = {
+    name: data?.user?.name ?? "",
+    email: data?.user?.email ?? "",
+    avatar: data?.user?.avatar ?? "",
+  }
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarFallback className="rounded-lg">{user.name.substring(0, 1)}</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">{user.name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarFallback className="rounded-lg">{user.name.substring(0, 1)}</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">{user.name}</span>
+                  <span className="truncate text-xs">{user.email}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger className="gap-2">
+                  {theme === "light"
+                    ? <Sun className="size-4" />
+                    : theme === "dark"
+                      ? <Moon className="size-4" />
+                      : <SunMoon className="size-4" />
+                  }
+                  Switch theme
+                </DropdownMenuSubTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuSubContent>
+                    <DropdownMenuRadioGroup value={theme} >
+                      {(["light", "dark", "system"] as const).map((thm) => (
+                        <DropdownMenuRadioItem
+                          key={thm}
+                          value={thm}
+                          className="capitalize"
+                          onSelect={() => setTheme(thm)}
+                        >
+                          {thm}
+                        </DropdownMenuRadioItem>
+                      ))}
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuSubContent>
+                </DropdownMenuPortal>
+              </DropdownMenuSub>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => mutate()}>
+              <LogOut className="size-4 mr-2" />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  )
+}
