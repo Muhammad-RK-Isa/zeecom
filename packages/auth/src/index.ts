14a205453bf2f6ea813f98fa07b788e811eb40c8
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-27 12:44:11 +0600

    feat: add auth package
    - configure lucia-auth v3

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,43 @@
+import { Lucia, TimeSpan } from "lucia";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { users, sessions, type UserEntity } from "@zeecom/db/schema";
+import { db } from "@zeecom/db/client";
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
+
+export const lucia = new Lucia(adapter, {
+  getSessionAttributes: (/* attributes */) => {
+    return {};
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      id: attributes.id,
+      name: attributes.name,
+      email: attributes.email,
+      emailVerified: attributes.emailVerified,
+      avatar: attributes.avatar,
+      role: attributes.role,
+      createdAt: attributes.createdAt,
+      updatedAt: attributes.updatedAt,
+    };
+  },
+  sessionExpiresIn: new TimeSpan(1, "d"),
+  sessionCookie: {
+    name: "session",
+    expires: true,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseSessionAttributes: DatabaseSessionAttributes;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseSessionAttributes { }
+interface DatabaseUserAttributes extends Omit<UserEntity, "password"> { }
