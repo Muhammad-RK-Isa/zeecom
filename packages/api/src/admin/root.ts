4de4fda89a5a5fa808e0bf78cf48b9362d0e7467
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-27 17:32:50 +0600

    chore: remove unused procedures
    - remove test
    - remove mirror
    - clean up unused imports

--- packages/api/src/admin/root.ts
+++ packages/api/src/admin/root.ts
@@ -1,13 +1,7 @@
-import { z } from "zod";
-
 import { authRouter } from "./routers/auth";
-import { createAdminRouter, publicAdminProcedure } from "./trpc";
+import { createAdminRouter } from "./trpc";
 
 export const adminRouter = createAdminRouter({
-  test: publicAdminProcedure.query(() => "Test passed!!!"),
-  mirror: publicAdminProcedure
-    .input(z.string())
-    .mutation(async ({ input }) => input),
   auth: authRouter,
 });
 
