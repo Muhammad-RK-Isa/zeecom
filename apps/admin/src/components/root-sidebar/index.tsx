8bbb8d0e3f9d6139e9f18e457eb56b913615b92c
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-29 02:31:28 +0600

    fix: remove default sidebar config; add usable routes

--- apps/admin/src/components/root-sidebar/index.tsx
+++ apps/admin/src/components/root-sidebar/index.tsx
@@ -1,63 +1,107 @@
+import { Link, useLocation } from "@tanstack/react-router"
+import type {
+  LucideIcon
+} from "lucide-react"
 import {
+  ChevronRight,
   Command,
-  Settings2,
+  Layers3,
+  Package,
+  Settings,
+  ShoppingCart,
+  UsersRound,
 } from "lucide-react"
 import * as React from "react"
-
-import { NavMain } from "~/components/root-sidebar/nav-main"
-import { NavSecondary } from "~/components/root-sidebar/nav-secondary"
 import { NavUser } from "~/components/root-sidebar/nav-user"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "~/components/ui/collapsible"
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "~/components/ui/sidebar"
+import type { FileRoutesByFullPath } from '../../routeTree.gen'
 
-const data = {
-  navMain: [
+interface NavItem {
+  title: string
+  path: keyof FileRoutesByFullPath
+  icon: LucideIcon
+  isActive?: boolean
+  items?: {
+    title: string
+    path: keyof FileRoutesByFullPath
+  }[]
+}
+
+export function RootSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const location = useLocation()
+
+  const navLinks: NavItem[] = [
     {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
+      title: "Orders",
+      path: "/orders",
+      icon: ShoppingCart,
+    },
+    {
+      title: "Products",
+      path: "/products",
+      icon: Package,
       items: [
         {
-          title: "General",
-          url: "#",
+          title: "Collections",
+          path: "/collections",
         },
         {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
+          title: "Categories",
+          path: "/categories",
+        }
+      ]
     },
-  ],
-  navSecondary: [
-    
-  ],
-  projects: [
-    
-  ],
-}
-export function RootSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    {
+      title: "Inventory",
+      path: "/inventory",
+      icon: Layers3,
+    },
+    {
+      title: "Customers",
+      path: "/customers",
+      icon: UsersRound,
+    },
+    {
+      title: "Settings",
+      path: "/settings",
+      icon: Settings,
+      items: [
+        {
+          title: "Store",
+          path: "/settings/store",
+        },
+        {
+          title: "Users",
+          path: "/settings/users",
+        },
+        {
+          title: "Profile",
+          path: "/settings/profile",
+        }
+      ]
+    }
+  ]
+
   return (
     <Sidebar variant="sidebar" {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="#">
+              <Link to="/">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                   <Command className="size-4" />
                 </div>
@@ -65,15 +109,62 @@ export function RootSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) 
                   <span className="truncate font-semibold">ZECOM</span>
                   <span className="truncate text-xs">Dashboard</span>
                 </div>
-              </a>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
-        {/* <NavProjects projects={data.projects} /> */}
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
+        <SidebarGroup>
+          <SidebarMenu>
+            {navLinks.map((item) => {
+              if (!item.items)
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <Link to={item.path}>
+                      <SidebarMenuButton
+                        isActive={location.pathname === item.path}
+                      >
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                )
+              return (
+                <Collapsible key={item.title} asChild>
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <Link to={item.path} className="group/collapsible">
+                        <SidebarMenuButton
+                          tooltip={item.title}
+                          isActive={location.pathname === item.path}
+                        >
+                          <item.icon />
+                          <span>{item.title}</span>
+                          <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                        </SidebarMenuButton>
+                      </Link>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items.map((subItem) => (
+                          <SidebarMenuSubItem key={subItem.title}>
+                            <SidebarMenuSubButton asChild isActive={location.pathname === subItem.path}>
+                              <Link to={subItem.path}>
+                                <span>{subItem.title}</span>
+                              </Link>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              )
+            })}
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter>
         <NavUser />
