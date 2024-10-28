0c0f79eac9bc85eba3dbb6097d6e7f1cde7735fa
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-29 02:42:01 +0600

    feat: add products router to adminRouter

--- packages/api/src/admin/root.ts
+++ packages/api/src/admin/root.ts
@@ -1,8 +1,10 @@
 import { authRouter } from "./routers/auth";
+import { productsRouter } from "./routers/products";
 import { createAdminRouter } from "./trpc";
 
 export const adminRouter = createAdminRouter({
   auth: authRouter,
+  products: productsRouter,
 });
 
 export type AdminRouter = typeof adminRouter;
