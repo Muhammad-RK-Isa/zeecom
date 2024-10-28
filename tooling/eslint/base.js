ae27ff5d73d7a8c2b474935a24ed70a324a283a6
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2024-10-28 22:33:33 +0600

    fix(lint): turn of some typescript-eslint rules

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -76,6 +76,8 @@ export default tseslint.config(
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/only-throw-error": "off",
       "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
     },
   },
   {
